d690ad68a50afa05d5168ebc473ca713dc5d5c02
mpinol <michaelp@unity3d.com>
2020-09-22 14:50:30 -0700

    Adding TcpConnector and MessageGeneration.

---
+++ MessageGeneration/UnityEditorScripts/PackageActionAutoGenEditorWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+ namespace RosMessageGeneration
+{
+    public class PackageActionAutoGenEditorWindow : PackageAutoGenEditorWindow
+    {
+        protected override string GenerationType
+        {
+            get { return "action"; }
+        }
+
+        protected override string FileExtension
+        {
+            get { return "action"; }
+        }
+
+        [MenuItem("RosMessageGeneration/Auto Generate Actions/Package Actions...", false, 21)]
+        private static void OpenWindow()
+        {
+            PackageActionAutoGenEditorWindow window = GetWindow<PackageActionAutoGenEditorWindow>(false, "Action Auto Generation", true);
+            window.minSize = new Vector2(750, 100);
+            window.maxSize = new Vector2(750, 100);
+            window.Show();
+        }
+
+        protected override List<string> Generate(string inPath, string outPath, string rosPackageName = "")
+        {
+            return ActionAutoGen.GenerateSingleAction(inPath, outPath, rosPackageName);
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ROS-TCP-Connector
+
