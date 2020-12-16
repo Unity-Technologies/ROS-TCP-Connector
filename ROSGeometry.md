9c4d7f9edd0bfbcb57800f8f3b01fe7651447d22
LaurieCheers-unity <73140792+LaurieCheers-unity@users.noreply.github.com>
2020-12-16 11:33:04 -0800

    Added ROSGeometry library

---
+++ ROSGeometry.md
@@ -0,0 +1,56 @@
+# ROSGeometry component
+
+In Unity, the X axis points right, Y up, and Z forward. ROS, on the other hand, supports various coordinate frames: in the most commonly-used one, X points forward, Y left, and Z up. In ROS terminology, this frame is called "FLU" (forward, left, up), whereas the Unity coordinate frame would be "RUF" (right, up, forward).
+
+The ROSGeometry namespace contains code to make it easier to work with these various coordinate frames - letting you be explicit about what coordinates a given value is in at compile time, and managing the conversions for you. It does this with two generic structs, `Vector3<C>` and `Quaternion<C>`. The type parameter C indicates the coordinate frame you're working in - either FLU, or RUF, or perhaps a more exotic frame such as NED (north, east, down) or ENU (east, north, up), commonly used in aviation.
+
+
+# Converting between frames:
+
+For example, if you need to convert an object's position into the FLU coordinate frame, you might write:
+
+    Vector3<FLU> rosPos = obj.transform.position.To<FLU>();
+   
+An explicit cast, or calling the constructor, would also produce the same result.
+
+    Vector3<FLU> rosPos2 = (Vector3<FLU>)obj.transform.position;
+    Vector3<FLU> rosPos3 = new Vector3<FLU>(obj.transform.position);
+
+To convert back, just access the "toUnity" property on the vector.
+
+    Vector3 unityPos = rosPos.toUnity;
+
+And the same functions apply for converting Quaternions.
+
+# Ros Message conversions:
+
+For convenience, `Vector3<C>` has an implicit conversion into all three of the main ROS position message types: Point, Point32 and Vector3. Similarly, `Quaternion<C>` has an implicit conversion to the ROS Quaternion message. Hence, writing 3d data into a message can often be as simple as writing:
+
+	Imu msg = new Imu();
+	msg.linear_acceleration = acceleration.To<FLU>();
+	msg.orientation = rigidbody.transform.rotation.To<FLU>();
+	msg.angular_velocity = rigidbody.angularVelocity.To<FLU>();
+	ros.Send("imu", msg);
+
+Note, the calls to `To<FLU>()` above are essential. Normal Unity Vector3s or Quaternions do NOT have these conversions. You need to explicitly select a coordinate frame before converting to a ROS message.
+
+Unity's standard Transform class also has a `To<C>()` extension method that returns a ROS Transform message. So sending a Transform message typically looks like:
+
+    ros.Send("topic", obj.transform.To<FLU>());
+
+# Converting incoming messages
+
+You can also convert Points, Point32s and Vector3s back into Unity coordinates. To convert a Point in coordinate space C directly into a Unity Vector3, you can write `From<C>`. For example:
+
+    void SubscriberCallback(Point p)
+	{
+	  transform.position = p.From<FLU>();
+	}
+
+Or, if you need to work with them in the FLU coordinate space for now, you can write:
+
+    Vector3<FLU> rosPos = p.As<FLU>();
+
+(Note that this does NOT do any coordinate conversion. It simply assumes the point is in the FLU coordinate frame already, and transfers it into an appropriate container.)
+
+And again, the same goes for converting a Quaternion message into a Unity Quaternion or `Quaternion<C>`.
---
+++ Runtime/ROSGeometry/CoordinateSpaces.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ROSGeometry
+{
+    public interface CoordinateSpace
+    {
+        Vector3 ConvertFromRUF(Vector3 v); // convert this vector from the Unity coordinate space into mine
+        Vector3 ConvertToRUF(Vector3 v); // convert from my coordinate space into the Unity coordinate space
+
+        Quaternion ConvertFromRUF(Quaternion q); // convert this quaternion from the Unity coordinate space into mine
+        Quaternion ConvertToRUF(Quaternion q); // convert from my coordinate space into the Unity coordinate space
+    }
+
+    //RUF is the Unity coordinate space, so no conversion needed
+    public class RUF : CoordinateSpace
+    {
+        public Vector3 ConvertFromRUF(Vector3 v) => v;
+        public Vector3 ConvertToRUF(Vector3 v) => v;
+        public Quaternion ConvertFromRUF(Quaternion q) => q;
+        public Quaternion ConvertToRUF(Quaternion q) => q;
+    }
+
+    public class FLU : CoordinateSpace
+    {
+        public Vector3 ConvertFromRUF(Vector3 v) => new Vector3(v.z, -v.x, v.y);
+        public Vector3 ConvertToRUF(Vector3 v) => new Vector3(-v.y, v.z, v.x);
+        public Quaternion ConvertFromRUF(Quaternion q) => new Quaternion(q.z, -q.x, q.y, -q.w);
+        public Quaternion ConvertToRUF(Quaternion q) => new Quaternion(-q.y, q.z, q.x, -q.w);
+    }
+
+    public class NED : CoordinateSpace
+    {
+        public Vector3 ConvertFromRUF(Vector3 v) => new Vector3(v.z, v.x, -v.y);
+        public Vector3 ConvertToRUF(Vector3 v) => new Vector3(v.y, -v.z, v.x);
+        public Quaternion ConvertFromRUF(Quaternion q) => new Quaternion(q.z, q.x, -q.y, -q.w);
+        public Quaternion ConvertToRUF(Quaternion q) => new Quaternion(q.y, -q.z, q.x, -q.w);
+    }
+
+    public class ENU : CoordinateSpace
+    {
+        public Vector3 ConvertFromRUF(Vector3 v) => new Vector3(v.x, v.z, v.y);
+        public Vector3 ConvertToRUF(Vector3 v) => new Vector3(v.x, v.z, v.y);
+        public Quaternion ConvertFromRUF(Quaternion q) => new Quaternion(q.x, q.z, q.y, -q.w);
+        public Quaternion ConvertToRUF(Quaternion q) => new Quaternion(q.x, q.z, q.y, -q.w);
+    }
+
+    public static class CoordinateSpaceExtensions
+    {
+        public static Vector3<C> To<C>(this Vector3 self)
+            where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>(self);
+        }
+
+        public static Quaternion<C> To<C>(this Quaternion self)
+            where C : CoordinateSpace, new()
+        {
+            return new Quaternion<C>(self);
+        }
+
+        public static Vector3<C> As<C>(this RosMessageTypes.Geometry.Point self) where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>((float)self.x, (float)self.y, (float)self.z);
+        }
+
+        public static Vector3 From<C>(this RosMessageTypes.Geometry.Point self) where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>((float)self.x, (float)self.y, (float)self.z).toUnity;
+        }
+
+        public static Vector3<C> As<C>(this RosMessageTypes.Geometry.Point32 self) where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>(self.x, self.y, self.z);
+        }
+
+        public static Vector3 From<C>(this RosMessageTypes.Geometry.Point32 self) where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>(self.x, self.y, self.z).toUnity;
+        }
+
+        public static Vector3<C> As<C>(this RosMessageTypes.Geometry.Vector3 self) where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>((float)self.x, (float)self.y, (float)self.z);
+        }
+
+        public static Vector3 From<C>(this RosMessageTypes.Geometry.Vector3 self) where C : CoordinateSpace, new()
+        {
+            return new Vector3<C>((float)self.x, (float)self.y, (float)self.z).toUnity;
+        }
+
+        public static Quaternion<C> As<C>(this RosMessageTypes.Geometry.Quaternion self) where C : CoordinateSpace, new()
+        {
+            return new Quaternion<C>((float)self.x, (float)self.y, (float)self.z, (float)self.w);
+        }
+
+        public static Quaternion From<C>(this RosMessageTypes.Geometry.Quaternion self) where C : CoordinateSpace, new()
+        {
+            return new Quaternion<C>((float)self.x, (float)self.y, (float)self.z, (float)self.w).toUnity;
+        }
+
+        public static RosMessageTypes.Geometry.Transform To<C>(this Transform transform) where C : CoordinateSpace, new()
+        {
+            return new RosMessageTypes.Geometry.Transform(new Vector3<C>(transform.position), new Quaternion<C>(transform.rotation));
+        }
+    }
+}
