497e44e40aab3e302561b179d46dcf0a876777b0
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2020-12-16 13:42:05 -0800

    The AssetImporters namespace moved in 2020.2

--- Editor/MessageGeneration/ScriptedSrvImporter.cs
+++ Editor/MessageGeneration/ScriptedSrvImporter.cs
@@ -3,8 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityEditor.AssetImporters;
 using UnityEngine;
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
+using UnityEditor.Experimental.AssetImporters;
+#endif
 
 [ScriptedImporter(1, "srv")]
 public class ScriptedSrvImporter : ScriptedImporter
