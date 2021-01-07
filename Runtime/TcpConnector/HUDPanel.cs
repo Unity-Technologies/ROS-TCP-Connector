d0b71d50641eeb01ef771d28a4784c5c35d01789
at669 <amanda.trang@unity3d.com>
2021-01-07 16:20:40 -0700

    Expandable HUD panel with message contents

--- Runtime/TcpConnector/HUDPanel.cs
+++ Runtime/TcpConnector/HUDPanel.cs
@@ -4,6 +4,10 @@ public class HUDPanel : MonoBehaviour
 {
     GUIStyle labelStyle;
     GUIStyle contentStyle;
+    bool viewSent = false;
+    bool viewRecv = false;
+    Vector2 scrollViewVector = Vector2.zero;
+    Rect scrollRect;
     
     internal bool isEnabled;
     internal string host;
@@ -14,6 +18,7 @@ public class HUDPanel : MonoBehaviour
 
     void Awake()
     {
+        // Define font styles
         labelStyle = new GUIStyle
         {
             alignment = TextAnchor.MiddleLeft,
@@ -27,21 +32,39 @@ public class HUDPanel : MonoBehaviour
             padding = new RectOffset(10, 0, 0, 5),
             normal = {textColor = Color.white},
         };
+
+        scrollRect = new Rect(0, 0, 335, 285);
     }
 
     void OnGUI() 
     {
         if (!isEnabled) return;
 
+        scrollViewVector = GUI.BeginScrollView(new Rect(0, 0, scrollRect.width + 15, 300), scrollViewVector, scrollRect);
         GUILayout.BeginVertical("box");
+        
+        // ROS IP Setup
         GUILayout.Label($"ROS IP:", labelStyle);
         GUILayout.Label(host, contentStyle);
+
+        // Last message sent
         GUILayout.Label($"Last Message Sent:", labelStyle);
         GUILayout.Label(lastMessageSentMeta, contentStyle);
-        GUILayout.Label(lastMessageSent, contentStyle);
+        viewSent = GUILayout.Toggle(viewSent, "View contents");
+        if (viewSent)
+            GUILayout.Label(lastMessageSent, contentStyle);
+
+        // Last message received
         GUILayout.Label($"Last Message Received:", labelStyle);
         GUILayout.Label(lastMessageReceivedMeta, contentStyle);
-        GUILayout.Label(lastMessageReceived, contentStyle);
+        viewRecv = GUILayout.Toggle(viewRecv, "View contents");
+        if (viewRecv)
+            GUILayout.Label(lastMessageReceived, contentStyle);
+            
         GUILayout.EndVertical();
+        GUI.EndScrollView();
+
+        if (GUILayoutUtility.GetLastRect().height > 1 && GUILayoutUtility.GetLastRect().width > 1)
+            scrollRect = GUILayoutUtility.GetLastRect();
     }
 }
