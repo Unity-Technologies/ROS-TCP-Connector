b1bacf5c3c1a1b0adc804eec30fb9ee25ac0fb4b
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-02-12 11:24:11 -0800

    Updated version number; prebuilt ObjectRecognitionMessages and OctomapMessages

---
+++ com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/ObjectRecognition/action/ObjectRecognitionActionResult.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Unity.Robotics.ROSTCPConnector.MessageGeneration;
+using RosMessageTypes.Std;
+using RosMessageTypes.Actionlib;
+
+namespace RosMessageTypes.ObjectRecognition
+{
+    public class ObjectRecognitionActionResult : ActionResult<ObjectRecognitionResult>
+    {
+        public const string RosMessageName = "object_recognition_msgs-master/ObjectRecognitionActionResult";
+
+        public ObjectRecognitionActionResult() : base()
+        {
+            this.result = new ObjectRecognitionResult();
+        }
+
+        public ObjectRecognitionActionResult(Header header, GoalStatus status, ObjectRecognitionResult result) : base(header, status)
+        {
+            this.result = result;
+        }
+        public override List<byte[]> SerializationStatements()
+        {
+            var listOfSerializations = new List<byte[]>();
+            listOfSerializations.AddRange(this.header.SerializationStatements());
+            listOfSerializations.AddRange(this.status.SerializationStatements());
+            listOfSerializations.AddRange(this.result.SerializationStatements());
+
+            return listOfSerializations;
+        }
+
+        public override int Deserialize(byte[] data, int offset)
+        {
+            offset = this.header.Deserialize(data, offset);
+            offset = this.status.Deserialize(data, offset);
+            offset = this.result.Deserialize(data, offset);
+
+            return offset;
+        }
+
+    }
+}
