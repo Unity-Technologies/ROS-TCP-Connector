680a1e27810d552a554d729c10ea98661d0c16f0
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-02-23 17:06:11 -0800

    PointStamped - doing it the obvious "dumb" way works best.

---
+++ com.unity.robotics.ros-tcp-connector/Runtime/MessageVisualization/DefaultVisualizers/Geometry/DefaultVisualizerPointStamped.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Robotics.ROSTCPConnector.MessageGeneration;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
+using UnityEngine;
+using MPointStamped = RosMessageTypes.Geometry.PointStamped;
+
+namespace Unity.Robotics.MessageVisualizers
+{
+    public class DefaultVisualizerPointStamped : BasicVisualizer<MPointStamped>
+    {
+        public float radius = 0.01f;
+
+        public override void Draw(MPointStamped message, MessageMetadata meta, Color color, string label, DebugDraw.Drawing drawing)
+        {
+            MessageVisualizations.Draw<FLU>(drawing, message.point, color, label, radius);
+        }
+
+        public override Action CreateGUI(MPointStamped message, MessageMetadata meta, DebugDraw.Drawing drawing) => () =>
+        {
+            MessageVisualizations.GUI(message.header);
+            MessageVisualizations.GUI(meta.topic, message.point);
+        };
+    }
+}
