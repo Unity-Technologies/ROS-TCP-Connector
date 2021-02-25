f419bbe60f73c33cfbb88be7d01fd46b8d656c1e
LaurieCheers-unity <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-02-25 11:36:43 -0800

    M prefix for all messages (#66)

---
+++ com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/PregeneratedMessages/Nav/action/MGetMapActionGoal.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Unity.Robotics.ROSTCPConnector.MessageGeneration;
+using RosMessageTypes.Std;
+using RosMessageTypes.Actionlib;
+
+namespace RosMessageTypes.Nav
+{
+    public class MGetMapActionGoal : ActionGoal<MGetMapGoal>
+    {
+        public const string RosMessageName = "nav_msgs/GetMapActionGoal";
+
+        public MGetMapActionGoal() : base()
+        {
+            this.goal = new MGetMapGoal();
+        }
+
+        public MGetMapActionGoal(MHeader header, MGoalID goal_id, MGetMapGoal goal) : base(header, goal_id)
+        {
+            this.goal = goal;
+        }
+        public override List<byte[]> SerializationStatements()
+        {
+            var listOfSerializations = new List<byte[]>();
+            listOfSerializations.AddRange(this.header.SerializationStatements());
+            listOfSerializations.AddRange(this.goal_id.SerializationStatements());
+            listOfSerializations.AddRange(this.goal.SerializationStatements());
+
+            return listOfSerializations;
+        }
+
+        public override int Deserialize(byte[] data, int offset)
+        {
+            offset = this.header.Deserialize(data, offset);
+            offset = this.goal_id.Deserialize(data, offset);
+            offset = this.goal.Deserialize(data, offset);
+
+            return offset;
+        }
+
+    }
+}
