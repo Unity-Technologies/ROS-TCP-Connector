e290af708b53da1c36dd7d958d7d4aff667c6702
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-02-26 10:53:03 -0800

    Renamed folder to message-visualizations

---
+++ com.unity.robotics.message-visualizations/Runtime/DefaultVisualizers/Trajectory/DefaultVisualizerJointTrajectory.cs
@@ -0,0 +1,29 @@
+﻿using RosMessageTypes.Trajectory;
+using RosSharp.Urdf;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Robotics.MessageVisualizers;
+using UnityEngine;
+
+public class DefaultVisualizerJointTrajectory : BasicVisualizer<MJointTrajectory>
+{
+    public UrdfRobot urdfRobot;
+    RobotVisualizationData robotData;
+
+    public override void Start()
+    {
+        base.Start();
+        robotData = new RobotVisualizationData(urdfRobot);
+    }
+
+    public override void Draw(MJointTrajectory message, MessageMetadata meta, Color color, string label, DebugDraw.Drawing drawing)
+    {
+        MessageVisualizations.Draw(drawing, message.joint_names, message.points, color);
+    }
+
+    public override Action CreateGUI(MJointTrajectory message, MessageMetadata meta, DebugDraw.Drawing drawing)
+    {
+        return base.CreateGUI(message, meta, drawing);
+    }
+}
