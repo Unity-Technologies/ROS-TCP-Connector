5802111fee231fef11640e109999091b9dda3f66
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-04-07 14:32:12 -0700

    Updated to latest KeepIncomingConnection changes

--- com.unity.robotics.ros-tcp-connector/Runtime/TcpConnector/ROSConnection.cs
+++ com.unity.robotics.ros-tcp-connector/Runtime/TcpConnector/ROSConnection.cs
@@ -300,6 +300,10 @@ namespace Unity.Robotics.ROSTCPConnector
             if (!networkStream.CanRead)
                 return;
 
+            byte[] timeoutBytes = new byte[4];
+            networkStream.Read(timeoutBytes, 0, 4);
+            float timeout = BitConverter.ToSingle(timeoutBytes, 0);
+
             SubscriberCallback subs;
 
             float lastDataReceivedRealTimestamp = 0;
@@ -348,7 +352,7 @@ namespace Unity.Robotics.ROSTCPConnector
                 }
                 await Task.Yield();
             }
-            while (Time.realtimeSinceStartup < lastDataReceivedRealTimestamp + 15);
+            while (Time.realtimeSinceStartup < lastDataReceivedRealTimestamp + timeout); // time out if idle too long.
             networkStream.Close();
         }
 
