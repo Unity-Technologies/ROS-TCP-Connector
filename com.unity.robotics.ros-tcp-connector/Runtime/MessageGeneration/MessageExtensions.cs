3065b4e9bae129a64bc03e72f6706dded3d64e4b
at669 <amanda.trang@unity3d.com>
2021-04-30 15:32:02 -0600

    Setting up Matrix Extensions

--- com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/MessageExtensions.cs
+++ com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/MessageExtensions.cs
@@ -322,192 +322,6 @@ namespace Unity.Robotics.ROSTCPConnector.MessageGeneration
             return tex;
         }
 
-        static double[][] MatrixCreate(int rows, int cols)
-        {
-            double[][] result = new double[rows][];
-            for (int i = 0; i < rows; ++i)
-                result[i] = new double[cols];
-            return result;
-        }
-
-        static double[][] MatrixDecompose(double[][] matrix, out int[] perm, out int toggle)
-        {
-            // Doolittle LUP decomposition with partial pivoting.
-            // rerturns: result is L (with 1s on diagonal) and U;
-            // perm holds row permutations; toggle is +1 or -1 (even or odd)
-            int rows = matrix.Length;
-            int cols = matrix[0].Length; // assume square
-            if (rows != cols)
-                throw new Exception("Attempt to decompose a non-square m");
-
-            int n = rows; // convenience
-
-            double[][] result = MatrixDuplicate(matrix); 
-
-            perm = new int[n]; // set up row permutation result
-            for (int i = 0; i < n; ++i) { perm[i] = i; }
-
-            toggle = 1; // toggle tracks row swaps.
-            // +1 -> even, -1 -> odd. used by MatrixDeterminant
-
-            for (int j = 0; j < n - 1; ++j) // each column
-            {
-                double colMax = Math.Abs(result[j][j]); // find largest val in col
-                int pRow = j;
-                //for (int i = j + 1; i < n; ++i)
-                //{
-                //  if (result[i][j] > colMax)
-                //  {
-                //    colMax = result[i][j];
-                //    pRow = i;
-                //  }
-                //}
-
-                // reader Matt V needed this:
-                for (int i = j + 1; i < n; ++i) 
-                {
-                if (Math.Abs(result[i][j]) > colMax)
-                {
-                    colMax = Math.Abs(result[i][j]);
-                    pRow = i;
-                }
-                }
-                // Not sure if this approach is needed always, or not.
-
-                if (pRow != j) // if largest value not on pivot, swap rows
-                {
-                double[] rowPtr = result[pRow];
-                result[pRow] = result[j];
-                result[j] = rowPtr;
-
-                int tmp = perm[pRow]; // and swap perm info
-                perm[pRow] = perm[j];
-                perm[j] = tmp;
-
-                toggle = -toggle; // adjust the row-swap toggle
-                }
-
-                // --------------------------------------------------
-                // This part added later (not in original)
-                // and replaces the 'return null' below.
-                // if there is a 0 on the diagonal, find a good row
-                // from i = j+1 down that doesn't have
-                // a 0 in column j, and swap that good row with row j
-                // --------------------------------------------------
-
-                if (result[j][j] == 0.0)
-                {
-                // find a good row to swap
-                int goodRow = -1;
-                for (int row = j + 1; row < n; ++row)
-                {
-                    if (result[row][j] != 0.0)
-                    goodRow = row;
-                }
-
-                if (goodRow == -1)
-                    throw new Exception("Cannot use Doolittle's method");
-
-                // swap rows so 0.0 no longer on diagonal
-                double[] rowPtr = result[goodRow];
-                result[goodRow] = result[j];
-                result[j] = rowPtr;
-
-                int tmp = perm[goodRow]; // and swap perm info
-                perm[goodRow] = perm[j];
-                perm[j] = tmp;
-
-                toggle = -toggle; // adjust the row-swap toggle
-                }
-                // --------------------------------------------------
-                // if diagonal after swap is zero . .
-                //if (Math.Abs(result[j][j]) < 1.0E-20) 
-                //  return null; // consider a throw
-
-                for (int i = j + 1; i < n; ++i)
-                {
-                result[i][j] /= result[j][j];
-                for (int k = j + 1; k < n; ++k)
-                {
-                    result[i][k] -= result[i][j] * result[j][k];
-                }
-                }
-
-
-            } // main j column loop
-
-            return result;
-        }
-
-        static double[][] MatrixDuplicate(double[][] matrix)
-        {
-            // allocates/creates a duplicate of a matrix.
-            double[][] result = MatrixCreate(matrix.Length, matrix[0].Length);
-            for (int i = 0; i < matrix.Length; ++i) // copy the values
-                for (int j = 0; j < matrix[i].Length; ++j)
-                result[i][j] = matrix[i][j];
-            return result;
-        }
-
-        static double[][] MatrixInverse(double[][] matrix)
-        {
-            int n = matrix.Length;
-            double[][] result = MatrixDuplicate(matrix);
-
-            int[] perm;
-            int toggle;
-            double[][] lum = MatrixDecompose(matrix, out perm,
-                out toggle);
-            if (lum == null)
-                throw new Exception("Unable to compute inverse");
-
-            double[] b = new double[n];
-            for (int i = 0; i < n; ++i)
-            {
-                for (int j = 0; j < n; ++j)
-                {
-                if (i == perm[j])
-                    b[j] = 1.0;
-                else
-                    b[j] = 0.0;
-                }
-
-                double[] x = HelperSolve(lum, b); // 
-
-                for (int j = 0; j < n; ++j)
-                result[j][i] = x[j];
-            }
-            return result;
-        }
-
-        static double[] HelperSolve(double[][] luMatrix, double[] b)
-        {
-            // before calling this helper, permute b using the perm array
-            // from MatrixDecompose that generated luMatrix
-            int n = luMatrix.Length;
-            double[] x = new double[n];
-            b.CopyTo(x, 0);
-
-            for (int i = 1; i < n; ++i)
-            {
-                double sum = x[i];
-                for (int j = 0; j < i; ++j)
-                sum -= luMatrix[i][j] * x[j];
-                x[i] = sum;
-            }
-
-            x[n - 1] /= luMatrix[n - 1][n - 1];
-            for (int i = n - 2; i >= 0; --i)
-            {
-                double sum = x[i];
-                for (int j = i + 1; j < n; ++j)
-                sum -= luMatrix[i][j] * x[j];
-                x[i] = sum / luMatrix[i][i];
-            }
-
-            return x;
-        }
-
         /// <summary>
         /// Finds the world coordinates of an image coordinate based on the CameraInfo matrices
         /// K matrix     P matrix
@@ -525,10 +339,6 @@ namespace Unity.Robotics.ROSTCPConnector.MessageGeneration
             MPoint worldCoord = new MPoint(x, y, 1);
 
             return new MPoint((float)invK[0][0]*worldCoord.x + (float)invK[0][1]*worldCoord.y + (float)invK[0][2]*worldCoord.z, (float)invK[1][0]*worldCoord.x + (float)invK[1][1]*worldCoord.y + (float)invK[1][2]*worldCoord.z, (float)invK[2][0]*worldCoord.x + (float)invK[2][1]*worldCoord.y + (float)invK[2][2]*worldCoord.z);
-            
-            // Vector3 t = new Vector3((float)P[0]*x + (float)P[1]*y + (float)P[2]*0 + (float)P[3]*1, (float)P[4]*x + (float)P[5]*y + (float)P[6]*0 + (float)P[7]*1, (float)P[8]*x + (float)P[9]*y + (float)P[10]*0 + (float)P[11]*1);
-
-            // return new Vector2(t.x / t.z, t.y / t.z);
         }
 
         public static MPoint[] GetPixelsInWorld(this MCameraInfo cameraInfo)
@@ -541,7 +351,7 @@ namespace Unity.Robotics.ROSTCPConnector.MessageGeneration
                 new double[] { cameraInfo.K[6], cameraInfo.K[7], cameraInfo.K[8] },
             };
 
-            var inverseK = MatrixInverse(formatK);
+            var inverseK = MatrixExtensions.MatrixInverse(formatK);
 
             for (int i = 0; i < cameraInfo.width; i++) 
             {
@@ -563,7 +373,7 @@ namespace Unity.Robotics.ROSTCPConnector.MessageGeneration
                 new double[] { cameraInfo.K[6], cameraInfo.K[7], cameraInfo.K[8] },
             };
 
-            var inverseK = MatrixInverse(formatK);
+            var inverseK = MatrixExtensions.MatrixInverse(formatK);
             for (int i = 0; i < tex.width; i++) 
             {
                 for (int j = 0; j < tex.height; j++)
