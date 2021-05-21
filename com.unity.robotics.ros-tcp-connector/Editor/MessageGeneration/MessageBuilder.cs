c24c128c0ea3b25c57ef512d0e7137baab05c199
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-05-21 09:41:14 -0700

    Windows build doesn't need to be standalone

--- com.unity.robotics.ros-tcp-connector/Editor/MessageGeneration/MessageBuilder.cs
+++ com.unity.robotics.ros-tcp-connector/Editor/MessageGeneration/MessageBuilder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector.MessageGeneration;
 using UnityEditor;
+using System.Runtime.InteropServices;
 
 public class MessageBuilder: ScriptableObject
 {
@@ -13,13 +14,14 @@ public class MessageBuilder: ScriptableObject
 #elif UNITY_EDITOR_LINUX
         string exePath = AssetDatabase.GUIDToAssetPath("f6548c1741bccda428981a0a40a6e275");
 #else
-        string exePath = AssetDatabase.GUIDToAssetPath("0c9b6f69171169246bf168b506904b8f");
+        //        string exePath = AssetDatabase.GUIDToAssetPath("0c9b6f69171169246bf168b506904b8f");
+        string exePath = AssetDatabase.GUIDToAssetPath("0b30214e32bb6e54c8dd1a4aea599867");
 #endif
         string exeName = System.IO.Path.GetFileName(exePath);
 
         System.Diagnostics.Process p = System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
         {
-            FileName = System.IO.Path.Combine(Application.dataPath, "..", exePath),
+            FileName = System.IO.Path.GetFullPath(exePath),
             Arguments = sourceFile + " " + targetFolder,
             UseShellExecute = false,
             RedirectStandardError = true,
