34656d1504da801bef2c2279f51dd2ce31f06c34
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-05-26 14:34:28 -0700

    Backwards compat message classes inherit from the correct class

--- com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/MessageBackwardsCompat.cs
+++ com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/MessageBackwardsCompat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RosMessageTypes.BuiltinInterfaces;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Robotics.ROSTCPConnector.MessageGeneration;
@@ -6,137 +7,137 @@ using UnityEngine;
 
 namespace RosMessageTypes.Diagnostic
 {
-    public class MDiagnosticArray : Message { }
-    public class MDiagnosticStatus : Message { }
-    public class MKeyValue : Message { }
+    public class MDiagnosticArray : DiagnosticArrayMsg { }
+    public class MDiagnosticStatus : DiagnosticStatusMsg { }
+    public class MKeyValue : KeyValueMsg { }
 }
 
 namespace RosMessageTypes.Geometry
 {
-    public class MAccel : Message { }
-    public class MAccelStamped : Message { }
-    public class MAccelWithCovariance : Message { }
-    public class MAccelWithCovarianceStamped : Message { }
-    public class MInertia : Message { }
-    public class MInertiaStamped : Message { }
-    public class MPoint32 : Message { }
-    public class MPoint : Message { }
-    public class MPointStamped : Message { }
-    public class MPolygon : Message { }
-    public class MPolygonStamped : Message { }
-    public class MPose2D : Message { }
-    public class MPoseArray : Message { }
-    public class MPose : Message { }
-    public class MPoseStamped : Message { }
-    public class MPoseWithCovariance : Message { }
-    public class MPoseWithCovarianceStamped : Message { }
-    public class MQuaternion : Message { }
-    public class MQuaternionStamped : Message { }
-    public class MTransform : Message { }
-    public class MTransformStamped : Message { }
-    public class MTwist : Message { }
-    public class MTwistStamped : Message { }
-    public class MTwistWithCovariance : Message { }
-    public class MTwistWithCovarianceStamped : Message { }
-    public class MVector3 : Message { }
-    public class MVector3Stamped : Message { }
-    public class MWrench : Message { }
-    public class MWrenchStamped : Message { }
+    public class MAccel : AccelMsg { }
+    public class MAccelStamped : AccelStampedMsg { }
+    public class MAccelWithCovariance : AccelWithCovarianceMsg { }
+    public class MAccelWithCovarianceStamped : AccelWithCovarianceStampedMsg { }
+    public class MInertia : InertiaMsg { }
+    public class MInertiaStamped : InertiaStampedMsg { }
+    public class MPoint32 : Point32Msg { }
+    public class MPoint : PointMsg { }
+    public class MPointStamped : PointStampedMsg { }
+    public class MPolygon : PolygonMsg { }
+    public class MPolygonStamped : PolygonStampedMsg { }
+    public class MPose2D : Pose2DMsg { }
+    public class MPoseArray : PoseArrayMsg { }
+    public class MPose : PoseMsg { }
+    public class MPoseStamped : PoseStampedMsg { }
+    public class MPoseWithCovariance : PoseWithCovarianceMsg { }
+    public class MPoseWithCovarianceStamped : PoseWithCovarianceStampedMsg { }
+    public class MQuaternion : QuaternionMsg { }
+    public class MQuaternionStamped : QuaternionStampedMsg { }
+    public class MTransform : TransformMsg { }
+    public class MTransformStamped : TransformStampedMsg { }
+    public class MTwist : TwistMsg { }
+    public class MTwistStamped : TwistStampedMsg { }
+    public class MTwistWithCovariance : TwistWithCovarianceMsg { }
+    public class MTwistWithCovarianceStamped : TwistWithCovarianceStampedMsg { }
+    public class MVector3 : Vector3Msg { }
+    public class MVector3Stamped : Vector3StampedMsg { }
+    public class MWrench : WrenchMsg { }
+    public class MWrenchStamped : WrenchStampedMsg { }
 }
 
 namespace RosMessageTypes.Nav
 {
-    public class MGridCells : Message { }
-    public class MMapMetaData : Message { }
-    public class MOccupancyGrid : Message { }
-    public class MOdometry : Message { }
-    public class MPath : Message { }
+    public class MGridCells : GridCellsMsg { }
+    public class MMapMetaData : MapMetaDataMsg { }
+    public class MOccupancyGrid : OccupancyGridMsg { }
+    public class MOdometry : OdometryMsg { }
+    public class MPath : PathMsg { }
 }
 
 namespace RosMessageTypes.Sensor
 {
-    public class MBatteryState : Message { }
-    public class MCameraInfo : Message { }
-    public class MChannelFloat32 : Message { }
-    public class MCompressedImage : Message { }
-    public class MFluidPressure : Message { }
-    public class MIlluminance : Message { }
-    public class MImage : Message { }
-    public class MImu : Message { }
-    public class MJointState : Message { }
-    public class MJoyFeedbackArray : Message { }
-    public class MJoyFeedback : Message { }
-    public class MJoy : Message { }
-    public class MLaserEcho : Message { }
-    public class MLaserScan : Message { }
-    public class MMagneticField : Message { }
-    public class MMultiDOFJointState : Message { }
-    public class MMultiEchoLaserScan : Message { }
-    public class MNavSatFix : Message { }
-    public class MNavSatStatus : Message { }
-    public class MPointCloud2 : Message { }
-    public class MPointCloud : Message { }
-    public class MPointField : Message { }
-    public class MRange : Message { }
-    public class MRegionOfInterest : Message { }
-    public class MRelativeHumidity : Message { }
-    public class MTemperature : Message { }
-    public class MTimeReference : Message { }
+    public class MBatteryState : BatteryStateMsg { }
+    public class MCameraInfo : CameraInfoMsg { }
+    public class MChannelFloat32 : ChannelFloat32Msg { }
+    public class MCompressedImage : CompressedImageMsg { }
+    public class MFluidPressure : FluidPressureMsg { }
+    public class MIlluminance : IlluminanceMsg { }
+    public class MImage : ImageMsg { }
+    public class MImu : ImuMsg { }
+    public class MJointState : JointStateMsg { }
+    public class MJoyFeedbackArray : JoyFeedbackArrayMsg { }
+    public class MJoyFeedback : JoyFeedbackMsg { }
+    public class MJoy : JoyMsg { }
+    public class MLaserEcho : LaserEchoMsg { }
+    public class MLaserScan : LaserScanMsg { }
+    public class MMagneticField : MagneticFieldMsg { }
+    public class MMultiDOFJointState : MultiDOFJointStateMsg { }
+    public class MMultiEchoLaserScan : MultiEchoLaserScanMsg { }
+    public class MNavSatFix : NavSatFixMsg { }
+    public class MNavSatStatus : NavSatStatusMsg { }
+    public class MPointCloud2 : PointCloud2Msg { }
+    public class MPointCloud : PointCloudMsg { }
+    public class MPointField : PointFieldMsg { }
+    public class MRange : RangeMsg { }
+    public class MRegionOfInterest : RegionOfInterestMsg { }
+    public class MRelativeHumidity : RelativeHumidityMsg { }
+    public class MTemperature : TemperatureMsg { }
+    public class MTimeReference : TimeReferenceMsg { }
 }
 
 namespace RosMessageTypes.Shape
 {
-    public class MMesh : Message { }
-    public class MMeshTriangle : Message { }
-    public class MPlane : Message { }
-    public class MSolidPrimitive : Message { }
+    public class MMesh : MeshMsg { }
+    public class MMeshTriangle : MeshTriangleMsg { }
+    public class MPlane : PlaneMsg { }
+    public class MSolidPrimitive : SolidPrimitiveMsg { }
 }
 
 namespace RosMessageTypes.Std
 {
-    public class MBool : Message { }
-    public class MByte : Message { }
-    public class MByteMultiArray : Message { }
-    public class MChar : Message { }
-    public class MColorRGBA : Message { }
-    public class MEmpty : Message { }
-    public class MFloat32 : Message { }
-    public class MFloat32MultiArray : Message { }
-    public class MFloat64 : Message { }
-    public class MFloat64MultiArray : Message { }
-    public class MInt16 : Message { }
-    public class MInt16MultiArray : Message { }
-    public class MInt32 : Message { }
-    public class MInt32MultiArray : Message { }
-    public class MInt64 : Message { }
-    public class MInt64MultiArray : Message { }
-    public class MInt8 : Message { }
-    public class MInt8MultiArray : Message { }
-    public class MMultiArrayDimension : Message { }
-    public class MMultiArrayLayout : Message { }
-    public class MString : Message { }
-    public class MUInt16 : Message { }
-    public class MUInt16MultiArray : Message { }
-    public class MUInt32 : Message { }
-    public class MUInt32MultiArray : Message { }
-    public class MUInt64 : Message { }
-    public class MUInt64MultiArray : Message { }
-    public class MUInt8 : Message { }
-    public class MUInt8MultiArray : Message { }
-    public class MHeader : Message { }
-    public class MTime : Message { }
-    public class MDuration : Message { }
+    public class MBool : BoolMsg { }
+    public class MByte : ByteMsg { }
+    public class MByteMultiArray : ByteMultiArrayMsg { }
+    public class MChar : CharMsg { }
+    public class MColorRGBA : ColorRGBAMsg { }
+    public class MEmpty : EmptyMsg { }
+    public class MFloat32 : Float32Msg { }
+    public class MFloat32MultiArray : Float32MultiArrayMsg { }
+    public class MFloat64 : Float64Msg { }
+    public class MFloat64MultiArray : Float64MultiArrayMsg { }
+    public class MInt16 : Int16Msg { }
+    public class MInt16MultiArray : Int16MultiArrayMsg { }
+    public class MInt32 : Int32Msg { }
+    public class MInt32MultiArray : Int32MultiArrayMsg { }
+    public class MInt64 : Int64Msg { }
+    public class MInt64MultiArray : Int64MultiArrayMsg { }
+    public class MInt8 : Int8Msg { }
+    public class MInt8MultiArray : Int8MultiArrayMsg { }
+    public class MMultiArrayDimension : MultiArrayDimensionMsg { }
+    public class MMultiArrayLayout : MultiArrayLayoutMsg { }
+    public class MString : StringMsg { }
+    public class MUInt16 : UInt16Msg { }
+    public class MUInt16MultiArray : UInt16MultiArrayMsg { }
+    public class MUInt32 : UInt32Msg { }
+    public class MUInt32MultiArray : UInt32MultiArrayMsg { }
+    public class MUInt64 : UInt64Msg { }
+    public class MUInt64MultiArray : UInt64MultiArrayMsg { }
+    public class MUInt8 : UInt8Msg { }
+    public class MUInt8MultiArray : UInt8MultiArrayMsg { }
+    public class MHeader : HeaderMsg { }
+    public class MTime : TimeMsg { }
+    public class MDuration : DurationMsg { }
 }
 
 namespace RosMessageTypes.Stereo
 {
-    public class MDisparityImage : Message { }
+    public class MDisparityImage : DisparityImageMsg { }
 }
 
 namespace RosMessageTypes.Trajectory
 {
-    public class MJointTrajectory : Message { }
-    public class MJointTrajectoryPoint : Message { }
-    public class MMultiDOFJointTrajectory : Message { }
-    public class MMultiDOFJointTrajectoryPoint : Message { }
+    public class MJointTrajectory : JointTrajectoryMsg { }
+    public class MJointTrajectoryPoint : JointTrajectoryPointMsg { }
+    public class MMultiDOFJointTrajectory : MultiDOFJointTrajectoryMsg { }
+    public class MMultiDOFJointTrajectoryPoint : MultiDOFJointTrajectoryPointMsg { }
 }
