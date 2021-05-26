64f24ac0f591948bbb3508e5ebcac8942af0d18f
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-05-26 12:08:49 -0700

    Backwards compat has all the pregenerated message types

--- com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/MessageBackwardsCompat.cs
+++ com.unity.robotics.ros-tcp-connector/Runtime/MessageGeneration/MessageBackwardsCompat.cs
@@ -4,41 +4,139 @@ using System.Collections.Generic;
 using Unity.Robotics.ROSTCPConnector.MessageGeneration;
 using UnityEngine;
 
-namespace RosMessageTypes.Geometry
+namespace RosMessageTypes.Diagnostic
 {
-    public class MPose: Message {}
-    public class MVector3 : Message {}
-    public class MPoint : Message { }
-    public class MPoint32 : Message { }
-    public class MQuaternion : Message { }
-    public class MTransform : Message { }
+    public class MDiagnosticArray : Message { }
+    public class MDiagnosticStatus : Message { }
+    public class MKeyValue : Message { }
 }
 
-namespace Unity.Robotics.ROSTCPConnector.ROSGeometry
+namespace RosMessageTypes.Geometry
 {
-    public static class BackwardsCompatExtensions
-    {
-        public static Vector3<C> As<C>(this RosMessageTypes.Geometry.MPoint self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
+    public class MAccel : Message { }
+    public class MAccelStamped : Message { }
+    public class MAccelWithCovariance : Message { }
+    public class MAccelWithCovarianceStamped : Message { }
+    public class MInertia : Message { }
+    public class MInertiaStamped : Message { }
+    public class MPoint32 : Message { }
+    public class MPoint : Message { }
+    public class MPointStamped : Message { }
+    public class MPolygon : Message { }
+    public class MPolygonStamped : Message { }
+    public class MPose2D : Message { }
+    public class MPoseArray : Message { }
+    public class MPose : Message { }
+    public class MPoseStamped : Message { }
+    public class MPoseWithCovariance : Message { }
+    public class MPoseWithCovarianceStamped : Message { }
+    public class MQuaternion : Message { }
+    public class MQuaternionStamped : Message { }
+    public class MTransform : Message { }
+    public class MTransformStamped : Message { }
+    public class MTwist : Message { }
+    public class MTwistStamped : Message { }
+    public class MTwistWithCovariance : Message { }
+    public class MTwistWithCovarianceStamped : Message { }
+    public class MVector3 : Message { }
+    public class MVector3Stamped : Message { }
+    public class MWrench : Message { }
+    public class MWrenchStamped : Message { }
+}
 
-        public static Vector3 From<C>(this RosMessageTypes.Geometry.MPoint self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
+namespace RosMessageTypes.Nav
+{
+    public class MGridCells : Message { }
+    public class MMapMetaData : Message { }
+    public class MOccupancyGrid : Message { }
+    public class MOdometry : Message { }
+    public class MPath : Message { }
+}
 
-        public static Vector3<C> As<C>(this RosMessageTypes.Geometry.MPoint32 self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
+namespace RosMessageTypes.Sensor
+{
+    public class MBatteryState : Message { }
+    public class MCameraInfo : Message { }
+    public class MChannelFloat32 : Message { }
+    public class MCompressedImage : Message { }
+    public class MFluidPressure : Message { }
+    public class MIlluminance : Message { }
+    public class MImage : Message { }
+    public class MImu : Message { }
+    public class MJointState : Message { }
+    public class MJoyFeedbackArray : Message { }
+    public class MJoyFeedback : Message { }
+    public class MJoy : Message { }
+    public class MLaserEcho : Message { }
+    public class MLaserScan : Message { }
+    public class MMagneticField : Message { }
+    public class MMultiDOFJointState : Message { }
+    public class MMultiEchoLaserScan : Message { }
+    public class MNavSatFix : Message { }
+    public class MNavSatStatus : Message { }
+    public class MPointCloud2 : Message { }
+    public class MPointCloud : Message { }
+    public class MPointField : Message { }
+    public class MRange : Message { }
+    public class MRegionOfInterest : Message { }
+    public class MRelativeHumidity : Message { }
+    public class MTemperature : Message { }
+    public class MTimeReference : Message { }
+}
 
-        public static Vector3 From<C>(this RosMessageTypes.Geometry.MPoint32 self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
+namespace RosMessageTypes.Shape
+{
+    public class MMesh : Message { }
+    public class MMeshTriangle : Message { }
+    public class MPlane : Message { }
+    public class MSolidPrimitive : Message { }
+}
 
-        public static Vector3<C> As<C>(this RosMessageTypes.Geometry.MVector3 self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
+namespace RosMessageTypes.Std
+{
+    public class MBool : Message { }
+    public class MByte : Message { }
+    public class MByteMultiArray : Message { }
+    public class MChar : Message { }
+    public class MColorRGBA : Message { }
+    public class MEmpty : Message { }
+    public class MFloat32 : Message { }
+    public class MFloat32MultiArray : Message { }
+    public class MFloat64 : Message { }
+    public class MFloat64MultiArray : Message { }
+    public class MInt16 : Message { }
+    public class MInt16MultiArray : Message { }
+    public class MInt32 : Message { }
+    public class MInt32MultiArray : Message { }
+    public class MInt64 : Message { }
+    public class MInt64MultiArray : Message { }
+    public class MInt8 : Message { }
+    public class MInt8MultiArray : Message { }
+    public class MMultiArrayDimension : Message { }
+    public class MMultiArrayLayout : Message { }
+    public class MString : Message { }
+    public class MUInt16 : Message { }
+    public class MUInt16MultiArray : Message { }
+    public class MUInt32 : Message { }
+    public class MUInt32MultiArray : Message { }
+    public class MUInt64 : Message { }
+    public class MUInt64MultiArray : Message { }
+    public class MUInt8 : Message { }
+    public class MUInt8MultiArray : Message { }
+    public class MHeader : Message { }
+    public class MTime : Message { }
+    public class MDuration : Message { }
+}
 
-        public static Vector3 From<C>(this RosMessageTypes.Geometry.MVector3 self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
+namespace RosMessageTypes.Stereo
+{
+    public class MDisparityImage : Message { }
+}
 
-        public static Quaternion<C> As<C>(this RosMessageTypes.Geometry.MQuaternion self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
-
-        public static Quaternion From<C>(this RosMessageTypes.Geometry.MQuaternion self) where C : ICoordinateSpace, new() => throw new NotImplementedException();
-
-        public static RosMessageTypes.Geometry.MTransform To<C>(this Transform transform) where C : ICoordinateSpace, new() => throw new NotImplementedException();
-
-        public static Vector3 From(this RosMessageTypes.Geometry.MPoint self, CoordinateSpaceSelection selection) => throw new NotImplementedException();
-        public static Vector3 From(this RosMessageTypes.Geometry.MPoint32 self, CoordinateSpaceSelection selection) => throw new NotImplementedException();
-        public static Vector3 From(this RosMessageTypes.Geometry.Vector3Msg self, CoordinateSpaceSelection selection) => throw new NotImplementedException();
-        public static Quaternion From(this RosMessageTypes.Geometry.MQuaternion self, CoordinateSpaceSelection selection) => throw new NotImplementedException();
-    }
+namespace RosMessageTypes.Trajectory
+{
+    public class MJointTrajectory : Message { }
+    public class MJointTrajectoryPoint : Message { }
+    public class MMultiDOFJointTrajectory : Message { }
+    public class MMultiDOFJointTrajectoryPoint : Message { }
 }
