f38babe37c30ae917f63df0a112ea280df57de9e
at669 <amanda.trang@unity3d.com>
2021-06-16 12:01:53 -0600

    Minor cleanup

--- com.unity.robotics.message-visualizations/Runtime/DefaultVisualizers/Sensor/DefaultVisualizerImage.cs
+++ com.unity.robotics.message-visualizations/Runtime/DefaultVisualizers/Sensor/DefaultVisualizerImage.cs
@@ -8,8 +8,6 @@ public class DefaultVisualizerImage : BasicTextureVisualFactory<MImage>
 {
     bool m_ConvertBgr = true;
     bool m_FlipY = true;
-    bool m_PrevConvert = true;
-    bool m_PrevFlip = true;
 
     public override Texture2D CreateTexture(MImage message)
     {
@@ -27,12 +25,6 @@ public class DefaultVisualizerImage : BasicTextureVisualFactory<MImage>
                 if (!message.encoding.Contains("1") && !message.encoding.Contains("mono") && !message.encoding.Contains("bayer")) m_ConvertBgr = GUILayout.Toggle(m_ConvertBgr, "From BGR");
                 m_FlipY = GUILayout.Toggle(m_FlipY, "Flip Y");
                 GUILayout.EndHorizontal();
-        
-                // TODO:
-                // if (m_ConvertBgr != m_PrevConvert || m_FlipY != m_PrevFlip)
-                // {
-                //     // m_Visualization.SetTexture(message.ToTexture2D(m_ConvertBgr, m_FlipY));
-                // }
             }
         
             // TODO: Rescale/recenter image based on window height/width
@@ -43,9 +35,6 @@ public class DefaultVisualizerImage : BasicTextureVisualFactory<MImage>
             }
         
             GUILayout.Label($"Height x Width: {message.height}x{message.width}\nEncoding: {message.encoding}");
-        
-            m_PrevConvert = m_ConvertBgr;
-            m_PrevFlip = m_FlipY;
         };
     }
 }
