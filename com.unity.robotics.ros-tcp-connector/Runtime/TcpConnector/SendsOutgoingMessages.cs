c5b686f839d9d43fdf4351817d157c2cf3c2b5ad
p3te <p96.smith@qut.edu.au>
2021-07-19 17:31:04 +1000

    Working to improve the ROS TCP connector by adding queues and latching.

---
+++ com.unity.robotics.ros-tcp-connector/Runtime/TcpConnector/SendsOutgoingMessages.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.IO;
+using Unity.Robotics.ROSTCPConnector.MessageGeneration;
+
+namespace Unity.Robotics.ROSTCPConnector
+{
+    public abstract class SendsOutgoingMessages
+    {
+
+        public enum SendToState
+        {
+            Normal,
+            NoMessageToSendError,
+            QueueFullWarning
+        }
+
+        public abstract SendToState SendTo(MessageSerializer m_MessageSerializer, System.IO.Stream stream);
+    }
+
+    public class SimpleDataSender : SendsOutgoingMessages
+    {
+        private List<byte[]> m_ListOfSerializations;
+
+        public SimpleDataSender(List<byte[]> m_ListOfSerializations)
+        {
+            this.m_ListOfSerializations = m_ListOfSerializations;
+        }
+
+        public override SendToState SendTo(MessageSerializer m_MessageSerializer, Stream stream)
+        {
+            foreach (byte[] statement in m_ListOfSerializations)
+            {
+                stream.Write(statement, 0, statement.Length);
+            }
+
+            return SendToState.Normal;
+        }
+    }
+}
---
+++ com.unity.robotics.ros-tcp-connector/Runtime/TcpConnector/SysCommand.cs
@@ -0,0 +1,93 @@
+using System.IO;
+using JetBrains.Annotations;
+using Unity.Robotics.ROSTCPConnector.MessageGeneration;
+using UnityEngine;
+
+namespace Unity.Robotics.ROSTCPConnector
+{
+    public abstract class SysCommand
+    {
+
+        public const string k_SysCommand_Log = "__log";
+        public const string k_SysCommand_Warning = "__warn";
+        public const string k_SysCommand_Error = "__error";
+        public const string k_SysCommand_ServiceRequest = "__request";
+        public const string k_SysCommand_ServiceResponse = "__response";
+        public const string k_SysCommand_Subscribe = "__subscribe";
+        public const string k_SysCommand_Publish = "__publish";
+        public const string k_SysCommand_RosService = "__ros_service";
+        public const string k_SysCommand_UnityService = "__unity_service";
+        public const string k_SysCommand_TopicList = "__topic_list";
+
+        public abstract string Command
+        {
+            get;
+        }
+
+        public virtual object BuildParam()
+        {
+            return this;
+
+        }
+        public void PopulateSysCommand(MessageSerializer messageSerializer)
+        {
+            messageSerializer.Clear();
+            // syscommands are sent as:
+            // 4 byte command length, followed by that many bytes of the command
+            // (all command names start with __ to distinguish them from ros topics)
+            messageSerializer.Write(Command);
+            // 4-byte json length, followed by a json string of that length
+            string json = JsonUtility.ToJson(BuildParam());
+            messageSerializer.WriteUnaligned(json);
+        }
+
+        public void SendTo([NotNull] Stream stream, MessageSerializer messageSerializer = null)
+        {
+            if (messageSerializer == null)
+            {
+                messageSerializer = new MessageSerializer();
+            }
+
+            PopulateSysCommand(messageSerializer);
+            messageSerializer.SendTo(stream);
+        }
+    }
+
+    public class SysCommandSubscriberRegistration : SysCommand
+    {
+        [SerializeField] public string topic;
+        [SerializeField] public string rosMessageName;
+
+        public SysCommandSubscriberRegistration(string topic, string rosMessageName)
+        {
+            this.topic = topic;
+            this.rosMessageName = rosMessageName;
+        }
+
+        public override string Command => k_SysCommand_Subscribe;
+    }
+
+    public class SysCommandPublisherRegistration : SysCommand
+    {
+        [SerializeField] public string topic;
+        [SerializeField] public string message_name;
+        [SerializeField] public int queue_size;
+        [SerializeField] public bool latch;
+
+        public SysCommandPublisherRegistration(ROSPublisherBase rosPublisher) : this(
+            rosPublisher.TopicName, rosPublisher.RosMessageName, rosPublisher.QueueSize, rosPublisher.Latch)
+        {
+        }
+
+        public SysCommandPublisherRegistration(string topic, string messageName, int queueSize, bool latch)
+        {
+            this.topic = topic;
+            message_name = messageName;
+            queue_size = queueSize;
+            this.latch = latch;
+        }
+
+        public override string Command => k_SysCommand_Publish;
+    }
+
+}
