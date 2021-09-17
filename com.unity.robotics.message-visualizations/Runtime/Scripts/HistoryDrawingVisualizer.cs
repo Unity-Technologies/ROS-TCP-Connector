a726a370ccfad44dabde1af87d080b3cc49e67ad
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-09-17 16:18:02 -0700

    MultiDrawingVisualizer -> HistoryDrawingVisualizer

--- com.unity.robotics.message-visualizations/Runtime/Scripts/HistoryDrawingVisualizer.cs
+++ com.unity.robotics.message-visualizations/Runtime/Scripts/HistoryDrawingVisualizer.cs
@@ -1,13 +1,14 @@
 using RosMessageTypes.Std;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Robotics.ROSTCPConnector;
 using Unity.Robotics.ROSTCPConnector.MessageGeneration;
 using UnityEngine;
 
 namespace Unity.Robotics.MessageVisualizers
 {
-    public abstract class MultiDrawingVisualizer<T> : BaseVisualFactory<T>
+    public abstract class HistoryDrawingVisualizer<T> : BaseVisualFactory<T>
         where T : Message
     {
         [SerializeField]
@@ -18,10 +19,15 @@ namespace Unity.Robotics.MessageVisualizers
 
         protected override IVisual CreateVisual(string topic)
         {
-            return new MultiDrawingVisual(topic, this, m_HistoryLength);
+            return new HistoryDrawingVisual(topic, this, m_HistoryLength);
         }
 
-        public virtual void Draw(Drawing3d drawing, IEnumerable<Tuple<T, MessageMetadata>> messages) { }
+        public virtual void Draw(Drawing3d drawing, IEnumerable<Tuple<T, MessageMetadata>> messages)
+        {
+            Draw(drawing, messages.Select(Tuple => Tuple.Item1));
+        }
+
+        public virtual void Draw(Drawing3d drawing, IEnumerable<T> messages) { }
 
         public virtual Action CreateGUI(IEnumerable<Tuple<T, MessageMetadata>> messages)
         {
@@ -38,19 +44,19 @@ namespace Unity.Robotics.MessageVisualizers
             return MessageVisualizationUtils.CreateDefaultGUI(message, meta);
         }
 
-        public class MultiDrawingVisual : IVisual
+        public class HistoryDrawingVisual : IVisual
         {
             string m_Topic;
-            Queue<Tuple<T, MessageMetadata>> messages = new Queue<Tuple<T, MessageMetadata>>();
+            Queue<Tuple<T, MessageMetadata>> m_History = new Queue<Tuple<T, MessageMetadata>>();
 
             Drawing3d m_BasicDrawing;
             Action m_GUIAction;
-            MultiDrawingVisualizer<T> m_Factory;
+            HistoryDrawingVisualizer<T> m_Factory;
             int m_HistoryLength;
             bool m_IsDrawingEnabled;
             float m_LastDrawingFrameTime = -1;
 
-            public MultiDrawingVisual(string topic, MultiDrawingVisualizer<T> factory, int historyLength)
+            public HistoryDrawingVisual(string topic, HistoryDrawingVisualizer<T> factory, int historyLength)
             {
                 m_Topic = topic;
                 m_Factory = factory;
@@ -64,12 +70,12 @@ namespace Unity.Robotics.MessageVisualizers
                 if (!MessageVisualizationUtils.AssertMessageType<T>(message, m_Topic))
                     return;
 
-                messages.Enqueue(new Tuple<T, MessageMetadata>(
+                m_History.Enqueue(new Tuple<T, MessageMetadata>(
                     (T)message,
                     new MessageMetadata(m_Topic, Time.time, DateTime.Now)
                 ));
-                if (messages.Count > m_HistoryLength)
-                    messages.Dequeue();
+                if (m_History.Count > m_HistoryLength)
+                    m_History.Dequeue();
                 m_GUIAction = null;
 
                 if (m_IsDrawingEnabled && Time.time > m_LastDrawingFrameTime)
@@ -96,7 +102,7 @@ namespace Unity.Robotics.MessageVisualizers
             {
                 if (m_GUIAction == null)
                 {
-                    m_GUIAction = m_Factory.CreateGUI(messages);
+                    m_GUIAction = m_Factory.CreateGUI(m_History);
                 }
                 m_GUIAction();
             }
@@ -122,7 +128,7 @@ namespace Unity.Robotics.MessageVisualizers
                     m_BasicDrawing.Clear();
                 }
 
-                m_Factory.Draw(m_BasicDrawing, messages);
+                m_Factory.Draw(m_BasicDrawing, m_History);
             }
         }
     }
